5a235191d86a116809eb59093f2c06d049dceb66
Jacqueline Cardozo (Valtech) <jacqueline.cardozo@valtech.com>
2020-08-11 16:37:29 -0300

    adding return statement

--- src/yourprojectname/scripts/components/hire-me-please/hire-me-please.vue
+++ src/yourprojectname/scripts/components/hire-me-please/hire-me-please.vue
@@ -63,7 +63,6 @@ export default {
       })
         .then(this.parseJSON)
         .then(data => {
-          console.log('data', data);
           this.answer = data.answer;
         })
         .catch(error => {
