a9559cbd953a2ebc7f04ad8aa23f7adc63fc77d9
Lucas Frassao <frassao.lucas@gmail.com>
2020-09-10 20:35:14 -0300

    finished configuring the base

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+
+import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App: React.FC = () => {
-  return <div className="App" />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Home />
+      <Footer />
+
+      <GlobalStyle />
+    </BrowserRouter>
+  );
 };
 
 export default App;
