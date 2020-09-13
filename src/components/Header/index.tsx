df093d933db2aadf4873dc209a83db3fa1961b28
Lucas Frassao <frassao.lucas@gmail.com>
2020-09-12 22:07:55 -0300

    finished menu and it's  animation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { Squash as Hamburger } from 'hamburger-react';
+
+import logoWhite from '../../assets/logo-white.svg';
+
+import { Container, HamburgerButton, Menu, Links } from './styles';
 
 const Header: React.FC = () => {
-  return <h2>Header</h2>;
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [hamburgerColor, setHamburgerColor] = useState('#111');
+
+  const toggleMenu = useCallback(() => {
+    if (!menuOpen) {
+      setMenuOpen(true);
+      setHamburgerColor('#fff');
+    } else {
+      setMenuOpen(false);
+      setHamburgerColor('#111');
+    }
+  }, [menuOpen]);
+
+  return (
+    <Container>
+      <Menu open={menuOpen}>
+        <div className="logo-img">
+          <img src={logoWhite} alt="valtech" />
+        </div>
+        <p>The Podcast</p>
+
+        <Links>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">The show</Link>
+          </li>
+          <li>
+            <Link to="/">Episodes</Link>
+          </li>
+          <li>
+            <Link to="/">Contact</Link>
+          </li>
+        </Links>
+      </Menu>
+
+      <HamburgerButton onClick={toggleMenu} open={menuOpen}>
+        <Hamburger size={24} color={hamburgerColor} />
+      </HamburgerButton>
+    </Container>
+  );
 };
 
 export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,34 +1,61 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface MenuProps {
+  open: boolean;
+}
 
 export const Container = styled.div`
+  position: relative;
+`;
+
+export const Menu = styled.div<MenuProps>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
+
+  height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  padding: 6.4em 0;
+  justify-content: center;
 
   background: var(--color-secondary);
-  border-top: 1px solid var(--dark-gray);
+  color: #fff;
+
+  transform: translateX(-100%);
+
+  transition: transform 0.4s;
+
+  ${props =>
+    props.open &&
+    css`
+      transform: translateX(0);
+    `}
 
   div.logo-img {
+    margin-top: 4.2em;
+
     &::after {
       content: '';
       display: block;
-      width: 12rem;
+      width: 9rem;
       height: 2px;
-      margin: 1rem auto;
+      margin: 1rem auto 0;
       background: var(--color-teriary);
     }
   }
 
-  p.copyright {
-    color: var(--light-gray);
+  p {
+    font-size: 2.4rem;
+    font-weight: 300;
   }
 `;
 
 export const Links = styled.ul`
   list-style: none;
-  margin: 6.4em 0 7.2em;
+  margin: 4.8em 0;
 
   li {
     text-align: center;
@@ -71,5 +98,21 @@ export const Links = styled.ul`
     & + li {
       margin-top: 2.4em;
     }
+  }
+`;
+
+export const HamburgerButton = styled.div<MenuProps>`
+  position: relative;
+  z-index: 9;
+
+  height: 4.8rem;
+
+  .hamburger-react {
+    ${props =>
+      props.open &&
+      css`
+        position: fixed;
+        top: 0;
+      `}
   }
 `;
