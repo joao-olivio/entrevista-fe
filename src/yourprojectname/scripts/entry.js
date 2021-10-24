b81dcf581ce1cfab96c51455dc680be20d7b0031
Paulo Wosniak <paulo.ferreira@before.com.br>
2021-10-23 20:43:51 -0400

    importing components

--- src/yourprojectname/scripts/entry.js
+++ src/yourprojectname/scripts/entry.js
@@ -2,9 +2,17 @@ import Vue from "vue";
 
 // Import Components
 import HireMePlease from "./components/hire-me-please/hire-me-please.vue";
+import CardEpisode from "./components/card-episode/card-episode.vue";
+import EpisodesTitleSection from "./components/episodes-title-section/episodes-title-section.vue";
+import EpisodesSection from "./components/episodes-section/episodes-section.vue";
+import TabPanel from "./components/tab-panel/tab-panel.vue";
 
 // Vue Declaration for Components
 Vue.component(HireMePlease.name, HireMePlease);
+Vue.component(CardEpisode.name, CardEpisode);
+Vue.component(EpisodesTitleSection.name, EpisodesTitleSection);
+Vue.component(EpisodesSection.name, EpisodesSection);
+Vue.component(TabPanel.name, TabPanel);
 
 // Import Directives
 import { backgroundDirective, ellipsis } from "./directives";
