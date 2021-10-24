3f10d4923b345ed1b3d217f8567ceafbb1ca5edc
Paulo Wosniak <paulo.ferreira@before.com.br>
2021-10-23 20:41:01 -0400

    episode card handlebar + component

---
+++ fractal/components/03_organisms/card/card.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "card",
+  context: {
+    episode: {
+      title: "How to keep your design consistent",
+      name: "Thomas Cardoso",
+      jobTitle: "Designer",
+      duration: "1h 50 min",
+      category: "design"
+    }
+  }
+};
