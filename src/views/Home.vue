c656fe2576d2f8ef24409b38e65ab095eb4b2fcf
Raphael <raphael.v.work@gmail.com>
2021-11-29 09:22:18 -0300

    Adding pages

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <VLastEpisodes />
+</template>
+
+<script>
+import VLastEpisodes from '@/components/organisms/VLastEpisodes'
+
+export default {
+  name: 'Home',
+  components: {
+    VLastEpisodes
+  }
+}
+</script>
---
+++ src/views/Podcasts.vue
@@ -0,0 +1,5 @@
+<template>
+  <div style="margin: 2.5rem 0 0 0;font-size: 2rem;text-align: center;">
+    All podcasts should appear here
+  </div>
+</template>
